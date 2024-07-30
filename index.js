5c683018845a20e49ff57597c23b80480ee66342
IvanR96 <irios96@hotmail.com>
2024-07-29 22:09:41 -0700

    rm

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+const port = 3000;
 
 
 //Step 3 - Make the styling show up.
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
-  res.sendFile(__dirname + 'index.ejs');
+  res.render('index.ejs');
 });
 
 app.post("/submit", (req, res) => {
